b4f9d29f600c1b1b383f7831311b1106aedb361d
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2026-01-16 10:27:56 +0530

    Add tests for non-leap year and year 2000

--- test_leap.py
+++ test_leap.py
@@ -2,7 +2,9 @@ from leap import is_leap
 
 def test_is_leap():
     assert is_leap(2024) == True
+    
 def test_not_leap():
     assert is_leap(2023) == False
+    
 def test_2000():
     assert is_leap(2000) == True
