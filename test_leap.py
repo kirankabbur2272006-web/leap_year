e5f1fd297fb8092b987346149f0691778a87612f
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2026-01-16 10:26:20 +0530

    Update test_leap.py

--- test_leap.py
+++ test_leap.py
@@ -1,6 +1,8 @@
 from leap import is_leap
 
 def test_is_leap():
-    assert is_leap(2024) == True      # Leap year
-    assert is_leap(2023) == False     # Not a leap year
-    assert is_leap(2000) == True      # Leap year
+    assert is_leap(2024) == True
+def test_not_leap():
+    assert is_leap(2023) == False
+def test_2000():
+    assert is_leap(2000) == True
