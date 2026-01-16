061f5ec5a04a8fab9462cee97409913cc0e46382
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2026-01-16 10:20:35 +0530

    Update leap year tests for 2024 and 2023

--- test_leap.py
+++ test_leap.py
@@ -1,6 +1,6 @@
 from leap import is_leap
 
 def test_is_leap():
-    assert is_leap(2020) == True      # Leap year
-    assert is_leap(1900) == False     # Not a leap year
+    assert is_leap(2024) == True      # Leap year
+    assert is_leap(2023) == False     # Not a leap year
     assert is_leap(2000) == True      # Leap year
